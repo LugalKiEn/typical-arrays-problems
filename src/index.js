b88ee389f98feb6e058b6408cfcf3fa9a2eb08b6
LugalKiEn <ulasik1996@gmail.com>
2021-03-09 23:33:17 +0300

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (arr) {
+  let minValue = 0;
+  if (arr !== null && arr !== undefined && arr.length !== 0) { 
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] < minValue) minValue = arr[i];
+    }
+    return minValue;
+  }  else return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (arr) {
+  let maxValue = 0;
+  if (arr !== null && arr !== undefined && arr.length !== 0) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > maxValue) maxValue = arr[i];
+   }
+    return maxValue;
+  } else return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (arr) {
+  let value = 0;
+    if (arr !== null && arr !== undefined && arr.length !== 0) {
+    for (let i = 0; i < arr.length; i++) {
+      value += arr[i]
+    }
+    return value / arr.length;
+  } else return 0;
 }
